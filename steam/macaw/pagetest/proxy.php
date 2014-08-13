1a47b435c9265e44044cd66df6f7f3273c0bb6f9
Ryan Quintal <ryan@Ryans-MBPR.local>
2014-08-13 19:08:27 -0400

    updated macaw stuff
    New CSS for those pages, and new Macaw file

--- steam/macaw/pagetest/proxy.php
+++
@@ -1,8 +0,0 @@
-<?php
-$key = trim(file_get_contents("./key.txt"));
-$url = $_GET['requrl'] . "&key=" . trim($key);
-$file = file_get_contents($url);
-
-#file_put_contents("./phpout", "url-" . $url . " file: " . $file . "\n", FILE_APPEND);
-echo $file;
-?>
